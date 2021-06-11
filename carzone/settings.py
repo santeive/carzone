b435db174a861317229a70e4e6ec2d26b49b084f
santeive <packo.ive@gmail.com>
2021-06-11 17:46:24 -0500

    CReated the Team model

--- carzone/settings.py
+++ carzone/settings.py
@@ -76,10 +76,13 @@ WSGI_APPLICATION = 'carzone.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_db',
+        'USER': 'blog',
+        'PASSWORD': 'postgres',
     }
 }
+
 
 
 # Password validation
