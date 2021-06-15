cb9b201a4f8129f58dbf50b4c3b5284c5de37754
santeive <packo.ive@gmail.com>
2021-06-15 14:38:31 -0500

    Implement RichTextEditor, MUltiselectFields & Admin customization

--- cars/models.py
+++ cars/models.py
@@ -1,5 +1,7 @@
 from django.db import models
 from datetime import datetime
+from ckeditor.fields import RichTextField
+from multiselectfield import MultiSelectField
 
 # Create your models here.
 class Car(models.Model):
@@ -94,7 +96,7 @@ class Car(models.Model):
     year = models.IntegerField(('year'), choices=year_choice)
     condition = models.CharField(max_length=100)
     price = models.IntegerField()
-    description = models.TextField(max_length=500)
+    description = RichTextField()
 
     car_photo = models.ImageField(upload_to='photos/%Y/%m/%d/')
     car_photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
@@ -102,13 +104,14 @@ class Car(models.Model):
     car_photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     car_photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
 
-    feautres = models.CharField(choices=features_choices, max_length=100)
+    feautres = MultiSelectField(choices=features_choices)
+
     body_style = models.CharField(max_length=100)
     engine = models.CharField(max_length=100)
     transmission = models.CharField(max_length=100)
     interior = models.CharField(max_length=100)
     miles = models.IntegerField()
-    doors = models.IntegerField(choices=door_choices)
+    doors = models.CharField(choices=door_choices, max_length=10)
 
     passengers = models.IntegerField()
     vin_no = models.CharField(max_length=100)
@@ -117,3 +120,6 @@ class Car(models.Model):
     no_of_owners = models.CharField(max_length=100)
     is_featured = models.BooleanField(default=False)
     created_date = models.DateTimeField(default=datetime.now, blank=True)
+
+    def __str__(self):
+        return self.car_title
